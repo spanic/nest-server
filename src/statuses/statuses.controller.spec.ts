bb8f68dc574809a197148fed2a5cb3f61c31bbcf
spanic <sadulin2011@yandex.ru>
2025-01-03 18:10:18 +0300

    Adds testing step via Jest, renamed default workflow

--- src/statuses/statuses.controller.spec.ts
+++ src/statuses/statuses.controller.spec.ts
@@ -1,16 +1,16 @@
-import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { RxDbService } from 'src/database/rx-db.service';
 import { StatusesController } from './statuses.controller';
 import { StatusesService } from './statuses.service';
+import { DatabaseModule } from '../database/database.module';
 
 describe('StatusesController', () => {
   let controller: StatusesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule],
       controllers: [StatusesController],
-      providers: [StatusesService, RxDbService, ConfigService],
+      providers: [StatusesService],
     }).compile();
 
     controller = module.get<StatusesController>(StatusesController);
--- src/statuses/statuses.service.spec.ts
+++ src/statuses/statuses.service.spec.ts
@@ -1,14 +1,14 @@
-import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { RxDbService } from 'src/database/rx-db.service';
 import { StatusesService } from './statuses.service';
+import { DatabaseModule } from '../database/database.module';
 
 describe('StatusesService', () => {
   let service: StatusesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StatusesService, RxDbService, ConfigService],
+      imports: [DatabaseModule],
+      providers: [StatusesService],
     }).compile();
 
     service = module.get<StatusesService>(StatusesService);
